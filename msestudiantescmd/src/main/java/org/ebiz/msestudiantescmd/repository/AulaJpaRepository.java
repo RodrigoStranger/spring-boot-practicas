8c41172055563dfb4136723dded02ddc619f34d9
Rodrigo Infanzon Acosta <rinfanzona@ulasalle.edu.pe>
2025-07-18 18:53:37 -0500

    oupdate

--- msestudiantescmd/src/main/java/org/ebiz/msestudiantescmd/repository/AulaJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package org.ebiz.msestudiantescmd.repository;
-
-import org.ebiz.msestudiantescmd.model.Aula;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AulaJpaRepository extends JpaRepository<Aula, Long> {
-}
---
+++ sistemadereservasdesalasdeestudiocmd/src/main/java/com/ulasalle/sistemadereservasdesalasdeestudiocmd/model/Estudiante.java
@@ -0,0 +1,34 @@
+package com.ulasalle.sistemadereservasdesalasdeestudiocmd.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "estudiantes")
+@Getter
+@Setter
+public class Estudiante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    @Column(name = "id_estudiante")
+    private Long id;
+
+    @Column(name = "codigo_estudiante")
+    private String codigo;
+
+    @Column(name = "nombre_estudiante")
+    private String nombre;
+
+    @Column(name = "apellido_estudiante")
+    private String apellido;
+
+    @Column(name = "correo_estudiante")
+    private String correo;
+
+    @Column(name = "habilitado_estudiante")
+    private int habilitado;
+
+}
